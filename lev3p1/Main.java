5a3f873c92703a989d05a6cf1c899e4e2678aa29
andreaba0 <andreabarchietto01@gmail.com>
2023-12-18 17:59:59 +0100

    fix test methods, fix logic

--- lev3p1/Main.java
+++ lev3p1/Main.java
@@ -1,35 +1,30 @@
-/******************************************************************************
-
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
-*******************************************************************************/
 import java.util.Queue;
 import java.util.LinkedList;
 public class Main
 {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println(solution(new int[][]{
-		    {0,1,1,0},
-		    {0,0,0,1},
-		    {1,1,0,0},
-		    {1,1,1,0}
-		    
-		}));
-	    System.out.println(solution(new int[][]{
+        int[][] map1 = new int[][]{
 	        {0,0,0,0,0,0},
 	        {1,1,1,1,1,0},
 	        {0,0,0,0,0,0},
 	        {0,1,1,1,1,1},
 	        {0,1,1,1,1,1},
 	        {0,0,0,0,0,0}
-	    }));
-	    System.out.println(solution(new int[][]{
+	    };
+        int[][] map2 = new int[][]{
+		    {0,1,1,0},
+		    {0,0,0,1},
+		    {1,1,0,0},
+		    {1,1,1,0}
+		    
+		};
+        int[][] map3 = new int[][]{
 	        {0,0},
 	        {1,0}
-	    }));
+	    };
+        printAssert(solution(map1), 11);
+        printAssert(solution(map2), 7);
+        printAssert(solution(map3), 3);
         System.out.println(solution(new int[][]{
 	        {0,1,0},
 	        {1,1,0},
@@ -47,6 +42,14 @@ public class Main
 	        {0,0,0,0,0,0},
 	        {0,0,0,0,0,0},
 	        {0,0,0,0,0,0},
+	        {0,0,0,0,0,0},
+	        {0,0,0,0,0,0},
+	        {0,0,0,0,0,0}
+	    }));
+        System.out.println(solution(new int[][]{
+	        {0,1,1,0,0,0},
+	        {1,0,0,0,0,0},
+	        {1,0,0,0,0,0},
 	        {0,0,0,0,0,0},
 	        {0,0,0,0,0,0},
 	        {0,0,0,0,0,0}
@@ -143,6 +146,12 @@ public class Main
             this.p=p;
         }
     };
+    static enum Direction {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    };
     static class WrapperNode {
         public Node head;
         public boolean hasJumped=false;
@@ -214,47 +223,51 @@ public class Main
             if(currentWrapper.hasJumped==true) continue;
 
             if(
-                isAPossiblePath(map, rows, cols, current.x+2,current.y)==1&&
-                hasWallBetween(current.x,current.y,current.x+2,current.y,map)==1
+                hasPassableWall(current.x,current.y,Direction.RIGHT,map)==1
+                //isAPossiblePath(map, rows, cols, current.x+2,current.y)==1&&
+                //hasWallBetween(current.x,current.y,current.x+2,current.y,map)==1
             ) {
                 WrapperNode tmp = new WrapperNode();
-                tmp.head = new Node(current.x+2,current.y, current);
+                tmp.head = new Node(current.x+1,current.y, current);
                 tmp.hasJumped=true;
                 queue.addLast(tmp);
             }
             if(
-                isAPossiblePath(map, rows, cols, current.x-2,current.y)==1&&
-                hasWallBetween(current.x,current.y,current.x-2,current.y,map)==1
+                hasPassableWall(current.x,current.y,Direction.LEFT,map)==1
+                //isAPossiblePath(map, rows, cols, current.x-2,current.y)==1&&
+                //hasWallBetween(current.x,current.y,current.x-2,current.y,map)==1
             ) {
                 WrapperNode tmp = new WrapperNode();
-                tmp.head = new Node(current.x-2,current.y, current);
+                tmp.head = new Node(current.x-1,current.y, current);
                 tmp.hasJumped=true;
                 queue.addLast(tmp);
             }
             if(
-                isAPossiblePath(map, rows, cols, current.x,current.y+2)==1&&
-                hasWallBetween(current.x,current.y,current.x,current.y+2,map)==1
+                hasPassableWall(current.x,current.y,Direction.DOWN,map)==1
+                //isAPossiblePath(map, rows, cols, current.x,current.y+2)==1&&
+                //hasWallBetween(current.x,current.y,current.x,current.y+2,map)==1
             ) {
                 WrapperNode tmp = new WrapperNode();
-                tmp.head = new Node(current.x,current.y+2, current);
+                tmp.head = new Node(current.x,current.y+1, current);
                 tmp.hasJumped=true;
                 queue.addLast(tmp);
             }
             if(
-                isAPossiblePath(map, rows, cols, current.x,current.y-2)==1&&
-                hasWallBetween(current.x,current.y,current.x,current.y-2,map)==1
+                hasPassableWall(current.x,current.y,Direction.UP,map)==1
+                //isAPossiblePath(map, rows, cols, current.x,current.y-2)==1&&
+                //hasWallBetween(current.x,current.y,current.x,current.y-2,map)==1
             ) {
                 WrapperNode tmp = new WrapperNode();
-                tmp.head = new Node(current.x,current.y-2, current);
+                tmp.head = new Node(current.x,current.y-1, current);
                 tmp.hasJumped=true;
                 queue.addLast(tmp);
             }
         }
-        System.out.println("Original map:");
+        /*System.out.println("Original map:");
         printMatrix(map);
         System.out.println("Visited map:");
         printMatrix(visited);
-        System.out.println(rootReversed.size()+" nodes reached the end");
+        System.out.println(rootReversed.size()+" nodes reached the end");*/
 
         int lowerDistance=-1;
         for(int i=0;i<rootReversed.size();i++) {
@@ -294,32 +307,51 @@ public class Main
         if(x>=0&&x<w&&y>=0&&y<h&&map[x][y]==0) return 1;
         return 0;
     }
-    static int hasWallBetween(int x1, int y1, int x2, int y2, int[][] map) {
-        if(x1<x2&&map[x1+1][y1]==1&&map[x1+2][y1]==0) return 1;
-        if(x1>x2&&map[x1-1][y1]==1&&map[x1-2][y1]==0) return 1;
-        if(y1<y2&&map[x1][y1+1]==1&&map[x1][y1+2]==0) return 1;
-        if(y1>y2&&map[x1][y1-1]==1&&map[x1][y1-2]==0) return 1;
-        return 0;
-    }
-    static int getDWithoutInterval(int x1, int y1, int x2, int y2, Node head) {
-        Node tmp = head;
-        int stop=0;
-        int d=0;
-        while(tmp!=null) {
-            if((tmp.x==x1&&tmp.y==y1)||(tmp.x==x2&&tmp.y==y2)) {
-                stop=(stop+1)%2;
-                d+=1;
-                if(stop==1) d+=1;
-                tmp=tmp.p;
-                continue;
-                
-            }
-            if(stop==0) {
-                d+=1;
-            }
-            tmp=tmp.p;
+
+    /**
+     * @param x1 x coordinate of the first point
+     * @param y1 y coordinate of the first point
+     * @param direction direction of the wall relative to bunny position (x1,y1)
+     * @return 1 if there is a wall between (x1,y1) and (x2,y2)
+     */
+    static int hasPassableWall (int x, int y, Direction direction, int[][] map) {
+        int rows = map.length;
+        int cols = map[0].length;
+        /*
+         *     0
+         *   0 W 0
+         * 0 W b W 0
+         *   0 W 0
+         *     0
+         * 0: possible path to follow
+         * W: wall that can be destroyed
+         * b: position of bunny
+         */
+        if(direction==Direction.LEFT) {
+            if(x-1>=0&&y-1>=0&&map[x-1][y]==1&&map[x-1][y-1]==0) return 1;
+            if(x-2>=0&&map[x-1][y]==1&&map[x-2][y]==0) return 1;
+            if(x-1>=0&&y+1<cols&&map[x-1][y]==1&&map[x-1][y+1]==0) return 1;
+            return 0;
         }
-        return d;
+        if(direction==Direction.RIGHT) {
+            if(x+1<rows&&y-1>=0&&map[x+1][y]==1&&map[x+1][y-1]==0) return 1;
+            if(x+2<rows&&map[x+1][y]==1&&map[x+2][y]==0) return 1;
+            if(x+1<rows&&y+1<cols&&map[x+1][y]==1&&map[x+1][y+1]==0) return 1;
+            return 0;
+        }
+        if(direction==Direction.UP) {
+            if(x-1>=0&&y-1>=0&&map[x][y-1]==1&&map[x-1][y-1]==0) return 1;
+            if(y-2>=0&&map[x][y-1]==1&&map[x][y-2]==0) return 1;
+            if(x+1<rows&&y-1>=0&&map[x][y-1]==1&&map[x+1][y-1]==0) return 1;
+            return 0;
+        }
+        if(direction==Direction.DOWN) {
+            if(x-1>=0&&y+1<cols&&map[x][y+1]==1&&map[x-1][y+1]==0) return 1;
+            if(y+2<cols&&map[x][y+1]==1&&map[x][y+2]==0) return 1;
+            if(x+1<rows&&y+1<cols&&map[x][y+1]==1&&map[x+1][y+1]==0) return 1;
+            return 0;
+        }
+        return 0;
     }
     static int isInsideShortestPath(int x1, int y1, int x2, int y2, Node head) {
         int counter=0;
@@ -346,6 +378,14 @@ public class Main
     static void printBit(int bit) {
         if(bit==0) System.out.print("\u001B[90m"+"\u25A0"+"\u001B[0m");
         else System.out.print("\u001B[32m"+"\u25A0"+"\u001B[0m");
+    }
+    static void printAssert(int value, int expected) {
+        for(int i=0;i<10;i++) System.out.print("=");
+        System.out.print(">  ");
+        if(value==expected) System.out.print("\u001B[32m"+"OK"+"\u001B[0m");
+        else System.out.print("\u001B[31m"+"FAIL"+"\u001B[0m");
+        if(value==expected) System.out.println();
+        else System.out.println(" (expected "+expected+", got "+value+")");
     }
 }
 
